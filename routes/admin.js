a1e5688e4d0f7eb89f3739322d7a1b931bf203bc
Nylon1 <71017473+Nylon1@users.noreply.github.com>
2025-05-24 12:46:19 +0100

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Content = require('../models/Content');
+const Content = require('../models/content');
 
 // Get all pending videos
 router.get('/admin/pending', async (req, res) => {
